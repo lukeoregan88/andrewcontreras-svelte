d01d3487bc8a2d788190bb4618efdc1754cb05f9
Luke O'Regan <luke@dda.co.uk>
2023-06-15 16:37:49 +0100

    Fixed sitemap to display video data

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,6 +1,6 @@
 import { PUBLIC_WP_JSON_URL } from '$env/static/public';
 export async function GET() {
-	const res = await fetch(PUBLIC_WP_JSON_URL + 'wp-json/wp/v2/posts?per_page=100');
+	const res = await fetch(PUBLIC_WP_JSON_URL + 'wp-json/video_feeds/v1/videos');
 	const posts = await res.json();
 
 	const res2 = await fetch(PUBLIC_WP_JSON_URL + 'wp-json/wp/v2/pages?per_page=100');
@@ -8,22 +8,44 @@ export async function GET() {
 
 	const urls = [
 		// @ts-ignore
-		...pages.map((page) => ({ loc: page.link })),
+		...pages.map((page) => ({ item: page, type: 'page' })),
 		// @ts-ignore
-		...posts.map((post) => ({ loc: post.link }))
+		...posts.map((post) => ({ item: post, type: 'post' }))
 	];
 
 	const urlset = urls.map((url) => {
-		const { loc } = url;
+		const { item, type } = url;
 
 		// @ts-ignore
+		let itemUrl = '';
+		if (type === 'post') {
+			// @ts-ignore
+			itemUrl = 'https://andrewcontreras.com/projects/' + item.slug + '/';
+		} else {
+			// @ts-ignore
+			itemUrl = item.link;
+		}
 		const regex = /(https:\/\/wp-admin\.andrewcontreras\.com\/)(?!wp-content\/uploads)/g;
-		const replacedLoc = loc.replace(regex, 'https://andrewcontreras.com/');
+		const replacedLoc = itemUrl.replace(regex, 'https://andrewcontreras.com/');
+
+		let videoXml = '';
+
+		if (type === 'post') {
+			videoXml = `
+      <video:video>
+        <video:thumbnail_loc>${item.featured_image}</video:thumbnail_loc>
+        <video:title>${item.title}</video:title>
+        <video:description>${item.excerpt.replace(/&/g, '&amp;')}</video:description>
+        <video:content_loc>${item.file_url}</video:content_loc>
+      </video:video>
+    `;
+		}
 
 		return `<url>
       <loc>${replacedLoc}</loc>
       <changefreq>daily</changefreq>
       <priority>0.7</priority>
+      ${videoXml}
     </url>`;
 	});
 
